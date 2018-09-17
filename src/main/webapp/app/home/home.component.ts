fd2edb9d70a9660e2587451e5e79d20eabcaee84
israelb <israel.berko@cellwize.com>
2018-09-18 00:08:30 +0300

    Home page to show all posts

--- src/main/webapp/app/home/home.component.ts
+++ src/main/webapp/app/home/home.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { NgbModalRef } from '@ng-bootstrap/ng-bootstrap';
-import { JhiEventManager } from 'ng-jhipster';
-
-import { LoginModalService, Principal, Account } from 'app/core';
+import { HttpErrorResponse, HttpResponse } from '@angular/common/http';
+import {PostService} from '../entities/post/post.service';
+import { IPost } from 'app/shared/model/post.model';
+import { Principal } from 'app/core';
+import { JhiAlertService } from 'ng-jhipster';
 
 @Component({
     selector: 'jhi-home',
@@ -10,31 +11,34 @@ import { LoginModalService, Principal, Account } from 'app/core';
     styleUrls: ['home.css']
 })
 export class HomeComponent implements OnInit {
-    account: Account;
-    modalRef: NgbModalRef;
 
-    constructor(private principal: Principal, private loginModalService: LoginModalService, private eventManager: JhiEventManager) {}
+    posts: IPost[] = [];
+    currentAccount: any;
+
+    constructor(private postService: PostService,
+        private jhiAlertService: JhiAlertService
+        private principal: Principal) {
+
+    }
+
+    loadAll() {
+        this.postService.query().subscribe(
+            (res: HttpResponse<IPost[]>) => {
+                this.posts = res.body;
+            },
+            (res: HttpErrorResponse) => this.onError(res.message)
+        );
+    }
 
     ngOnInit() {
+        this.loadAll();
         this.principal.identity().then(account => {
-            this.account = account;
-        });
-        this.registerAuthenticationSuccess();
-    }
-
-    registerAuthenticationSuccess() {
-        this.eventManager.subscribe('authenticationSuccess', message => {
-            this.principal.identity().then(account => {
-                this.account = account;
-            });
+            this.currentAccount = account;
         });
     }
 
-    isAuthenticated() {
-        return this.principal.isAuthenticated();
+    private onError(errorMessage: string) {
+        this.jhiAlertService.error(errorMessage, null, null);
     }
 
-    login() {
-        this.modalRef = this.loginModalService.open();
-    }
 }
